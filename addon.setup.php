1880e0f91f8bcf6a7a6ea0601ae8163da0506c5b
Doug Black <dblack@americanbible.org>
2017-07-17 14:52:12 -0400

    Moved plugin info to addon.setup

---
+++ addon.setup.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+	'name' => 'Stringy',
+	'version' => '1.1',
+	'author' => 'Glenn Jacobs',
+	'author_url' => 'http://glennjacobs.co.uk/',
+	'description' => 'A helpful collection of string manipulation functions.',
+	'settings_exist' => false,
+	'namespace' => 'Stringy'
+);
